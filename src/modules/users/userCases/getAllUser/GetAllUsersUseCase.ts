677e43eeecf13686f089deb5b10d34652f2b2c91
Jackson Rios <juniorrios1231@gmail.com>
2023-02-07 18:22:00 -0300

    feat: create allUsers ✔

---
+++ src/modules/users/userCases/getAllUser/GetAllUsersUseCase.ts
@@ -0,0 +1,21 @@
+import { User } from '@prisma/client'
+import { prisma } from '../../../../prisma/client'
+
+export class GetAllUsersUseCase {
+    async execute(): Promise<User[]> {
+        const users = await prisma.user.findMany({
+            include: {
+                movie_rent: {
+                    select: {
+                        movie: {
+                            select: {
+                                title: true,
+                            },
+                        },
+                    },
+                },
+            },
+        })
+        return users
+    }
+}
