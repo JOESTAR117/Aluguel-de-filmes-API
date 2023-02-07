d1b7c56c52524e13df93e07be824813823af3fd6
Jackson Rios <juniorrios1231@gmail.com>
2023-02-07 19:00:53 -0300

    feat: project finished✔❤

--- src/routes/movie.routes.ts
+++ src/routes/movie.routes.ts
@@ -1,13 +1,20 @@
 import { Router } from 'express'
 import { CreateMovieController } from '../modules/movies/useCases/createMovie/CreateMovieController'
+import { GetMoviesByReleaseDateController } from '../modules/movies/useCases/getMoviesByReleaseDate/GetMoviesByReleaseDateController'
 import { CreateMovieRentController } from '../modules/moviesRent/createMovieRent/CreateMovieRentController'
 
 const createMovieController = new CreateMovieController()
+
+const getMoviesByReleaseDateController = new GetMoviesByReleaseDateController()
+
 const createMovieRentController = new CreateMovieRentController()
 
 const movieRoutes = Router()
 
 movieRoutes.post('/register', createMovieController.handle)
+
+movieRoutes.get('/release', getMoviesByReleaseDateController.handle)
+
 movieRoutes.post('/rent', createMovieRentController.handle)
 
 export { movieRoutes }
