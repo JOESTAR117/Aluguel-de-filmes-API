06d3c5d77b7c2c60a2cb3340ad1f5b0b1366dbae
Jackson Rios <juniorrios1231@gmail.com>
2023-02-07 18:36:06 -0300

    feat: GET users finished✔'

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express'
 import { CreateUserController } from '../modules/users/userCases/createUser/createUserController'
+import { GetAllUsersController } from '../modules/users/userCases/getAllUser/GetAllUsersController'
 
 const createUserController = new CreateUserController()
+const getAllUsersController = new GetAllUsersController()
 
 const userRoutes = Router()
 
 userRoutes.post('/register', createUserController.handle)
+userRoutes.get('/', getAllUsersController.handle)
 
 export { userRoutes }
